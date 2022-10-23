02b08e37b7da1551ccde548626c47b78a2292992
Daurenbek Beknazarov <drenbek@gmail.com>
2022-10-23 22:49:15 +0600

    added new commands to the webserver

--- examples/WebServer/src/main/java/org/example/Processor.java
+++ examples/WebServer/src/main/java/org/example/Processor.java
@@ -22,16 +22,37 @@ public class Processor {
         System.out.println(request.toString());
         System.out.flush();
 
+        String requestLine = request.getRequestLine().split(" ")[1];
+
         // To send response back to the client.
         PrintWriter output = new PrintWriter(socket.getOutputStream());
 
         // We are returning a simple web page now.
+
         output.println("HTTP/1.1 200 OK");
         output.println("Content-Type: text/html; charset=utf-8");
         output.println();
         output.println("<html>");
         output.println("<head><title>Hello</title></head>");
-        output.println("<body><p>Hello, world!</p></body>");
+        switch (requestLine) {
+            case "/create/itemid" -> {
+                output.println("<body><p>Create item</p></body>");
+            }
+            case "/delete/itemid" -> {
+                output.println("<body><p>Delete item</p></body>");
+            }
+            case "/exec/params" -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                output.println("<body><p>Execute parameters</p></body>");
+            }
+            default -> {
+                output.println("<body><p>Hello, world!</p></body>");
+            }
+        }
         output.println("</html>");
         output.flush();
 
